44563e178767f9c80c021f275eac15acfca94586
Vitaly <61142441+VitalyBibik@users.noreply.github.com>
2020-07-30 22:02:57 +0300

    fix link

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,6 +1,6 @@
 // const newsApiKey = '994e1bb6bcfe481cb84ed7ee5767eb2e';
 const serverUrl = NODE_ENV === 'development' ? 'https://newsapi.org/v2/everything?q=' : 'https://praktikum.tk/news/v2/everything?q=';
-const myServer = NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://backend-mesto.xyz';
+const myServer = NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.backend-mesto.xyz';
 
 const obj = {
     baseUrl: `${serverUrl}`,
